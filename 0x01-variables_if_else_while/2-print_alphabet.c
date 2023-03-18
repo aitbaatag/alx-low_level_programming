cce9149fa0f65bd2a28708d3383ad0d4361e15fc
khalid ait baatag <aitbaatagkhalid@gmail.com>
2023-03-17 17:58:14 -0700

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char az = 'a';
+while 
+(az <= 'z')
+{
+putchar(az);
+az++;
+}
+putchar('\n');
+}
