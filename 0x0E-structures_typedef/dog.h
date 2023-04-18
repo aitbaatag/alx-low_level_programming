4c6bde455d0de7838ee4177ba82edd1b021b0055
khalid ait baatag <aitbaatagkhalid@gmail.com>
2023-04-17 17:28:13 -0700

    Update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
- * struct dog - a dog struct
- * @name: dog's name
- * @age: dog's age
- * @owner: dog owner's name
- * Description: dog struct containing the name, age and owner's name.
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+char *name;
+float age;
+char *owner;
 };
+
 
 #endif
